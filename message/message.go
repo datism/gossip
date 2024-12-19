cc3ecda8a6dab641998af7f0c3090d57c717eaae
TrinhDat <69208070+datism@users.noreply.github.com>
2024-12-19 17:59:48 +0700

    Update message.go

--- message/message.go
+++ message/message.go
@@ -21,12 +21,42 @@ type Response struct {
 	ReasonPhrase string
 }
 
+type SIPUri struct {
+	Scheme string
+	User string
+	Pass string
+	Domain string
+	Port int
+	Opts map[string][string]
+	ExtOpts maps[string][string]
+}
+
+type SIPVia struct {
+	Proto string
+	Domain string
+	Port int
+	Branch string
+	Opts map[string][string]
+}
+
+type SIPCseq struct {
+	Method string
+	Seq int
+}
+
 // SIPMessage represents a SIP message
 type SIPMessage struct {
 	Startline
+	From SIPUri
+	To SIPUri
+	CallID string
+	Contacts []SIPUri
+	TopmostVia SIPVia
 	Headers map[string][]string
 	Body    []byte
 }
+
+
 
 // GetHeader returns the values of a specific header
 func GetHeader(msg *SIPMessage, header string) ([]string) {
@@ -86,6 +116,53 @@ func GetValueALWS(str string) string {
 	return str[index+1:]
 }
 
+// SetParam sets the value of a specific param in a header, or adds the param if it doesn't exist.
+func SetParam(header string, param string, value string) string {
+	// Find the position of "param" in the header
+	start := strings.Index(header, param)
+	if start == -1 {
+		// Parameter not found, add it to the header
+		return header + ";" + param + "=" + value
+	}
+
+	// Move the start position to the beginning of the parameter's value
+	start += len(param) + 1
+
+	// Find the next ";" to locate the end of the current param value
+	end := strings.Index(header[start:], ";")
+	if end == -1 {
+		// If no ";" is found, the value is till the end of the string
+		return header[:start] + value
+	} else {
+		// Otherwise, replace the value between the start and end positions
+		return header[:start] + value + header[start+end:]
+	}
+}
+
+// SetValueBLWS sets the value before the first space in the string.
+func SetValueBLWS(str, value string) string {
+	// Find the position of the first space
+	index := strings.Index(str, " ")
+	if index == -1 {
+		// If there is no space, return the value as the new string
+		return value
+	}
+	// Replace the part before the space with the new value
+	return value + str[index:]
+}
+
+// SetValueALWS sets the value after the first space in the string.
+func SetValueALWS(str, value string) string {
+	// Find the position of the first space
+	index := strings.Index(str, " ")
+	if index == -1 {
+		// If there is no space, return the value since nothing to set after
+		return ""
+	}
+	// Replace the part after the space with the new value
+	return str[:index+1] + value
+}
+
 func MakeGeneralResponse(status_code int, reason string, request *SIPMessage) (*SIPMessage) {
 	req_hdr := request.Headers
 	res_hdr :=  make(map[string][]string)
@@ -101,4 +178,57 @@ func MakeGeneralResponse(status_code int, reason string, request *SIPMessage) (*
 		Startline: Startline{Response: &Response{StatusCode: status_code, ReasonPhrase: reason},},
 		Headers: res_hdr,
 	}
+}
+
+func MakeGeneralAck(inv *SIPMessage, res *SIPMessage) (*SIPMessage) {
+	/* RFC 3261 17.1.1.3
+ 	The ACK request constructed by the client transaction MUST contain
+  	values for the Call-ID, From, and Request-URI that are equal to the
+   	values of those header fields in the request passed to the transport
+   	by the client transaction (call this the "original request").
+
+    	The To header field in the ACK MUST equal the To header field in the
+   	response being acknowledged, and therefore will usually differ from
+   	the To header field in the original request by the addition of the
+   	tag parameter.
+
+    	The ACK MUST contain a single Via header field, and
+  	this MUST be equal to the top Via header field of the original
+   	request.
+
+    	The CSeq header field in the ACK MUST contain the same
+   	value for the sequence number as was present in the original request,
+   	but the method parameter MUST be equal to "ACK".
+
+    	If the INVITE request whose response is being acknowledged had Route
+  	header fields, those header fields MUST appear in the ACK.  This is
+   	to ensure that the ACK can be routed properly through any downstream
+   	stateless proxies.
+ 	*/
+	inv_hdr := inv.Headers
+	ack_hdr := make(map[string][]string)
+
+	for _, key := range []string{"call-id", "to", "session-id"} {
+        if value, exists := inv_hdr[key]; exists { 
+            ack_hdr[key] = value
+        }
+
+	to := GetHeader(res, "to")
+	ack_hdr["to"] = to
+
+	vias := GetHeader(inv, "via")
+	ack_hdr["vias"] = []string{vias[0]}
+
+	cseq := GetHeader(inv, "cseq")
+	ack_cseq := SetValueALWS(cseq, "ACK")
+	ack_hdr["cseq"] = []string{ack_cseq}
+	
+	routes := GetHeader(inv, "route")
+	ack_hdr["route"] = routes
+
+	return &SIPMessage{
+		Startline: Startline{Requeest: &Request{Method: "ACK", RequestURI: inv.Request.RequestURI},},
+		Headers: ack_hdr,
+	}
+    }
 }
