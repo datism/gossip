777382cd94cdda11b654ad14102e4faa780c9066
TrinhDat <69208070+datism@users.noreply.github.com>
2024-12-20 14:40:12 +0700

    Update uri.go

--- message/uri/uri.go
+++ message/uri/uri.go
@@ -10,81 +10,125 @@ type SIPUri struct {
 	Pass    string
 	Domain  string
 	Port    int
-	Opts    map[string][]string
-	ExtOpts map[string][]string
+	Opts    map[string]string
+	Headers map[string]string
 }
 
-func Parse(raw_uri string) *SIPUri {
+func Parse(uri string) *SIPUri {
 	var sip_uri SIPUri
 	sip_uri.Opts = make(map[string][]string)
 
-	var ui_hp_up string
-	var ui_hp string
+	var ui_hp_up_hd string
+	var hp_up_hd string
+	var up_hd string
+
 	var user_info string
 	var host_port string
 	var opts string
-	var ex_opts string
+	var headers string
 
-	if strings.HasPrefix(raw_uri, "<") {
-		closing_angle_idx := strings.LastIndex(raw_uri, ">")
-		if closing_angle_idx == -1 {
+	colon_idx := strings.Index(uri, ":")
+	if colon_idx == -1 {
+		return nil
+	} else {
+		scheme = uri[1:colon_idx]
+		if scheme != "sip" && scheme == "sips" && scheme != "tel" {
 			return nil
-		}
-
-		ui_hp_up = raw_uri[1:closing_angle_idx]
-		ex_opts = raw_uri[closing_angle_idx+1:]
-	} else {
-		ui_hp_up = ""
-		sc_idx := strings.Index(raw_uri, ";")
-		if sc_idx == -1 {
-			ui_hp = raw_uri
-			ex_opts = ""
 		} else {
-			ui_hp = raw_uri[1:sc_idx]
-			ex_opts = raw_uri[sc_idx:]
+			sip_uri.Scheme = scheme
+			ui_hp_up_hd = uri[colon_idx+1:]
 		}
 	}
 
-	if ui_hp_up != "" {
-		sc_idx := strings.Index(raw_uri, ";")
-		if sc_idx == -1 {
-			ui_hp = ui_hp_up
-			opts = ""
-		} else {
-			ui_hp = ui_hp_up[1:sc_idx]
-			opts = ui_hp_up[sc_idx:]
-		}
-	}
-
-	at_idx := strings.Index(ui_hp, "@")
+	at_idx := strings.Index(ui_hp_up_hd, "@")
 	if at_idx == -1 {
-		host_port = ui_hp
 		user_info = ""
+		hp_up_hd = ui_hp_up_hd
 	} else {
-		user_info = ui_hp[1:at_idx]
-		host_port = ui_hp[at_idx:]
+		user_info = ui_hp_up_hd[1:at_idx]
+		hp_up_hd = ui_hp_up_hd[at_idx+1:]
+	}
+
+	seco_idx := strings.Index(hp_up_hd, ";")
+	if seco_idx == -1 {
+		host_port = hp_up_hd
+		up_hd = ""
+	} else {
+		host_port = hp_up_hd[1:seco_idx]
+		up_hd = hp_up_hd[seco_idx+1:]
+	}
+
+	ques_idx := strings.Index(up_hd, "?")
+	if ques_idx == -1 {
+		opts = up_hd
+		headers = ""
+	} else {
+		opts = up_hd[1:ques_idx]
+		headers = up_hd[ques_idx+1:]
 	}
 
 	parse_user_info(user_info, &sip_uri)
 	parse_host_port(host_port, &sip_uri)
 	parse_opts(opts, &sip_uri)
-	parse_ext_opts(ex_opts, &sip_uri)
+	parse_headers(headers, &sip_uri)
 
 	return &sip_uri
 }
 
 func parse_user_info(user_info string, uri *SIPUri) {
+	if user_info == "" {
+		return
+	}
 
+	colon_idx := strings.Index(user_info, ":")
+	if colon_idx == -1 {
+		uri.User = user_info
+	} else {
+		uri.User = user_info[1:colon_idx]
+		uri.Pass = user_info[colon_idx+1:]
+	}
 }
 
 func parse_host_port(host_port string, uri *SIPUri) {
+	if host_port == "" {
+		return
+	}
 
+	colon_idx := strings.Index(host_port, ":")
+	if colon_idx == -1 {
+		uri.Domain = host_port
+	} else {
+		uri.Domain = user_info[1:colon_idx]
+		uri.Port = user_info[colon_idx+1:]
+	}
 }
 
 func parse_opts(opts string, uri *SIPUri) {
+	if opts == "" {
+		return
+	}
 
+	uri.Opts = make(map[string]string)
+
+	for _, kvs := range strings.Split(opts, ";") {
+		kv := strings.SplitN(kvs, "=", 2)
+		if len(kv) == 2 {
+			uri.Opts[kv[0]] = kv[1]
+		}
+	}
 }
 
-func parse_ext_opts(ex_opts string, uri *SIPUri) {
+func parse_headers(headers string, uri *SIPUri) {
+	if headers == "" {
+		return
+	}
 
+	uri.Headers = make(map[string]string)
+
+	for _, kvs := range strings.Split(opts, "&") {
+		kv := strings.SplitN(kvs, "=", 2)
+		if len(kv) == 2 {
+			uri.Headers[kv[0]] = kv[1]
+		}
+	}
 }
