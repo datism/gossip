6109085c520923f1a3173e74006f880922ab1f90
TrinhDat <69208070+datism@users.noreply.github.com>
2025-01-02 17:59:51 +0700

    Update proxy_core.go

--- core/proxy_core.go
+++ core/proxy_core.go
@@ -1,9 +1,39 @@
 package core
 
-// import (
-// 	"gossip/transaction"
-// 	"github.com/rs/zerolog/log"
-// )
+import (
+	"gossip/transaction"
+  "gossip/message"
+  "gossip/util"
+	"github.com/rs/zerolog/log"
+)
+
+type proxy struct {
+	Chan chan util.Event
+	client_trans transaction.Transaction
+	server_trans transaction.Transaction
+	
+}
+
+func Make(request *message.SIPmessage) { 
+	ch = make(chan util.Event, 3)
+  tid, err := transaction.MakeTransactionID(msg)
+	if err != nil {
+		log.Error().Err(err).Msg("Cannot create transaction ID")
+		return
+	}
+
+  core_cb = func(from transaction.Transaction, ev util.Event) {
+    ch 
+  }
+
+  StartServerTransaction(tid, request, trpt_cb, core_cb)
+  
+  
+}
+
+func start() {
+  
+}
 
 // func MessageProcessing(transChan chan transaction.Event) {
 // 	for event := range transChan {
